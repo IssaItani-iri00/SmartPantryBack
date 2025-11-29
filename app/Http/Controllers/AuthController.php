d7a01c47e5dfb67738756dbec46c8fe8fa58ac2d
Issa Itani <>
2025-11-29 20:53:19 +0200

    made authcontroller requiring user to join or create a user when registering + tested it

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,15 +37,40 @@ class AuthController extends Controller {
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
+            'household_action' => 'required|in:create,join',
+            'household_name' => 'required_if:household_action,create|string|max:255',
+            'household_code' => 'required_if:household_action,join|string',
         ]);
+
         $user = new User;
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
         $user->save();
 
+        if ($request->household_action === 'create') {
+            $household = new Household;
+            $household->name = $request->household_name;
+            $household->save();
+            
+            $user->households()->attach($household->id);
+        } else {
+            $household = Household::where('invite_code', $request->household_code)->first();
+            
+            if (!$household) {
+                $user->delete();
+                return $this->responseJSON(null, "Invalid household code", 404);
+            }
+            
+            $user->households()->attach($household->id);
+        }
+
         $token = Auth::login($user);
         $user->token = $token;
+        
+        // Load household relationship
+        $user->load('households');
+        
         return $this->responseJSON($user);
     }
 
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\User\HouseHoldController;
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/households', [HouseHoldController::class, 'index']);
+    Route::post('/households', [HouseHoldController::class, 'store']);
+    Route::post('/households/join', [HouseHoldController::class, 'join']);
+});
+
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout']);
