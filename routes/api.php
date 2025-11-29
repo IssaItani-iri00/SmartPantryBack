c25e16f0c5ed9a7b39e9d200a4959db026ded8f0
Issa Itani <>
2025-11-29 21:45:47 +0200

    added user controller to send current user data

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\User\HouseHoldController;
+use App\Http\Controllers\User\UserController;
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user/me', [UserController::class, 'me']);
     Route::get('/households', [HouseHoldController::class, 'index']);
     Route::post('/households', [HouseHoldController::class, 'store']);
     Route::post('/households/join', [HouseHoldController::class, 'join']);
