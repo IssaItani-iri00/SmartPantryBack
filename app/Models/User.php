dbc5340d7737cdb6fb4ef2199c8377000e7bc764
Issa Itani <>
2025-11-29 13:04:56 +0200

    updated JWT library and tested login/register

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-use Tymon\JWTAuth\Contracts\JWTSubject;
-
-
-class User extends Authenticatable
-{
+class User extends Authenticatable implements JWTSubject {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
@@ -41,21 +38,33 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
+    protected function casts(): array {
         return [
             'password' => 'hashed',
         ];
     }
 
-    function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    function households()
-    {
+    function households() {
         return $this->belongsToMany(HouseHold::class)
                     ->withTimestamps();
     }
+
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims() {
+        return [];
+    }
+
 }
